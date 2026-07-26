3db5378ca52779e1347a50b1cbe8e6b5814ceb29
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-26 12:00:20 +0600

    announcemnet-done

---
+++ Js/Script.js
@@ -0,0 +1,4 @@
+//  Close Button
+ document.getElementById("closeBtn").addEventListener("click", function () {
+            document.getElementById("announcementBar").style.display = "none";
+        });
